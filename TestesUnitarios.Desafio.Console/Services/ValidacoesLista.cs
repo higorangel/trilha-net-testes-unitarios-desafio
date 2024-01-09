d8f7212b6d7ebd8e606642161fea72ec0dd1807c
higorangel <higgorgguees_@hotmail.com>
2024-01-09 15:26:07 -0300

    Testes unitarios concluídogit add TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs!

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -1,3 +1,4 @@
+
 namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
@@ -28,6 +29,11 @@ namespace TestesUnitarios.Desafio.Console.Services
         public int RetornarMenorNumeroLista(List<int> lista)
         {
             return lista.Min();
+        }
+
+        public bool ListaNaoContemDeterminadoNumero(List<int> lista, int numeroParaProcurar)
+        {
+            return (false);
         }
     }
 }
